214f75cb3c16d649600f35da8a974cc4936c2c4b
MikeTheFyke <mikefyke@hotmail.com>
2020-06-13 14:20:54 -0400

    Completed 30X30 signs funciton

--- logic/signLogic.js
+++ logic/signLogic.js
@@ -30,5 +30,26 @@ function signs30X30() {
     }
     if (strUser = "30X30" && strUser2 == ".40GA"){
         calculateWeight = .58 * numberOfSigns;
+    }
+    if (strUser = "30X30" && strUser2 == ".064GA"){
+        calculateWeight = .901 * numberOfSigns;
     } 
+    if (strUser = "30X30" && strUser2 == ".081GA"){
+        calculateWeight = 1.41 * numberOfSigns;
+    } 
+    if (strUser = "30X30" && strUser2 == "1.25GA"){
+        calculateWeight = 1.8 * numberOfSigns;
+    } 
+    if (strUser = "30X30" && strUser2 == "16GA"){
+        calculateWeight = 2.65 * numberOfSigns;
+    }
+    if (strUser = "30X30" && strUser2 == "1/2PLY"){
+        calculateWeight = 1.25 * numberOfSigns;
+    }
+    if (strUser = "30X30" && strUser2 == "1/4MDO"){
+        calculateWeight = 2.25 * numberOfSigns;
+    }
+    if (strUser = "30X30" && strUser2 == ".090POLY"){
+        calculateWeight = .75 * numberOfSigns;
+    }    
 }
