83eae10102ee96efa32468dd8a500bfef91c235a
MikeTheFyke <mikefyke@hotmail.com>
2020-06-13 14:05:32 -0400

    Added logic for 30X30 signs

--- logic/signLogic.js
+++ logic/signLogic.js
@@ -8,11 +8,16 @@ var strUser2;
 function weightCalculator () {
     signsSize = document.getElementById("signsSize");
     signsType = document.getElementById("signsType");
+    numberOfSigns = document.getElementById("signsNumberInput").value;
+
     strUser = signsSize.options[signsSize.selectedIndex].value;
     strUser2 = signsType.options[signsType.selectedIndex].value;
 
+    if (strUser = "30X30"){
+        calculateWeight = .29 * numberOfSigns;
+        console.log(numberOfSigns);
+    }
 
-
-    document.getElementById("totalWeight").innerHTML = strUser;
+    document.getElementById("totalWeight").innerHTML = calculateWeight;
 }
 
