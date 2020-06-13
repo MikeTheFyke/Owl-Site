73f7a6e765452bc69ff3ec334940e272aa315c5d
MikeTheFyke <mikefyke@hotmail.com>
2020-06-13 14:31:19 -0400

    Added 30X45 function

--- logic/signLogic.js
+++ logic/signLogic.js
@@ -17,8 +17,8 @@ function weightCalculator () {
         signs30X30();
     }
 
-    if (strUser = "30X45" && strUser2 == ".20GA"){
-        calculateWeight = .43 * numberOfSigns;
+    if (strUser = "30X45"){
+        signs30X45 ();
     }
 
     document.getElementById("totalWeight").innerHTML = calculateWeight;
@@ -46,10 +46,99 @@ function signs30X30() {
     if (strUser2 == "1/2PLY"){
         calculateWeight = 1.25 * numberOfSigns;
     }
-    if (strUser2 == "1/4MDO"){
+    if (strUser2 == "3/4MDO"){
         calculateWeight = 2.25 * numberOfSigns;
     }
     if (strUser2 == ".090POLY"){
         calculateWeight = .75 * numberOfSigns;
     }    
+}
+
+function signs30X45 (){
+    if (strUser2 == ".20GA"){
+        calculateWeight = .43 * numberOfSigns;
+    }
+    if (strUser2 == ".40GA"){
+        calculateWeight = .86 * numberOfSigns;
+    }
+    if (strUser2 == ".064GA"){
+        calculateWeight = 1.35 * numberOfSigns;
+    }
+    if (strUser2 == ".081GA"){
+        calculateWeight = 1.71 * numberOfSigns;
+    }
+    if (strUser2 == "1.25GA"){
+        calculateWeight = 2.7 * numberOfSigns;
+    } 
+    if (strUser2 == "16GA"){
+        calculateWeight = 3.98 * numberOfSigns;
+    }
+    if (strUser2 == "1/2PLY"){
+        calculateWeight = 1.88 * numberOfSigns;
+    }
+    if (strUser2 == "3/4MDO"){
+        calculateWeight = 3.38 * numberOfSigns;
+    }
+    if (strUser2 == ".090POLY"){
+        calculateWeight = .75 * numberOfSigns;
+    } 
+}
+
+function signs30X60 (){
+    
+}
+
+function signs30X90 (){
+    
+}
+
+function signs30X120 (){
+    
+}
+
+function signs45X45 (){
+    
+}
+
+function signs45X60 (){
+    
+}
+
+function signs45X75 (){
+    
+}
+
+function signs45X90 (){
+    
+}
+
+function signs60X60 (){
+    
+}
+
+function signs60X75 (){
+    
+}
+
+function signs60X90 (){
+    
+}
+
+function signs75X75 (){
+    
+}
+
+function signs75X90 (){
+    
+}
+
+function signs90X90 (){
+    
+}
+function signs90X120 (){
+    
+}
+
+function signs120X120 (){
+    
 }
