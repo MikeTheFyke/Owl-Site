06a2cbb791a3b971c962740f24841f71b5d7440c
MikeTheFyke <mikefyke@hotmail.com>
2020-06-13 16:19:00 -0400

    Completed 45X60 function

--- logic/signLogic.js
+++ logic/signLogic.js
@@ -265,7 +265,33 @@ function signs45X45 (){
 }
 
 function signs45X60 (){
-    
+    if (strUser2 == ".20GA"){
+        calculateWeight = .85 * numberOfSigns;
+    }
+    if (strUser2 == ".40GA"){
+        calculateWeight = 1.73 * numberOfSigns;
+    }
+    if (strUser2 == ".064GA"){
+        calculateWeight = 2.7 * numberOfSigns;
+    }
+    if (strUser2 == ".081GA"){
+        calculateWeight = 3.42 * numberOfSigns;
+    }
+    if (strUser2 == "1.25GA"){
+        calculateWeight = 5.4 * numberOfSigns;
+    } 
+    if (strUser2 == "16GA"){
+        calculateWeight = 7.9 * numberOfSigns;
+    }
+    if (strUser2 == "1/2PLY"){
+        calculateWeight = 3.75 * numberOfSigns;
+    }
+    if (strUser2 == "3/4MDO"){
+        calculateWeight = 6.75 * numberOfSigns;
+    }
+    if (strUser2 == ".090POLY"){
+        calculateWeight = 1.5 * numberOfSigns;
+    } 
 }
 
 function signs45X75 (){
