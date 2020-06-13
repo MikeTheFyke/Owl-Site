9564adf7859207954769b618d8e252fb74e8020c
MikeTheFyke <mikefyke@hotmail.com>
2020-06-12 21:04:14 -0400

    started to added weight output logic

--- logic/signLogic.js
+++ logic/signLogic.js
@@ -1,5 +1,9 @@
 var calculateWeight = 1;
 
+var e = document.getElementById("signsSize");
+var strUser = e.option[e.selectedIndex].text;
+
 function weightCalculator () {
-    document.getElementById("totalWeight").innerHTML = calculateWeight;
+    document.getElementById("totalWeight").innerHTML = strUser;
 }
+
