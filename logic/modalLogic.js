d843fcc95b233690c0ff3c9673229446dc511b6a
MikeTheFyke <mikefyke@hotmail.com>
2020-06-24 16:35:48 -0400

    Added close and open logic to modalLogic

--- logic/modalLogic.js
+++ logic/modalLogic.js
@@ -1,3 +1,19 @@
 var modal = document.getElementById("receiptModal");
 
 var btn = document.getElementById("receiptButton");
+
+var span = document.getElementByClassName("close")[0];
+
+function openReceiptModal () {
+    modal.style.display = "block";
+}
+
+function closeReceiptModal() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
